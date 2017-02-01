1b20952ef76f6ccdbffe1f9df837d71db7b947cd
edramos <eduardorams@yahoo.com>
2017-02-01 07:43:35 -0500

    Add Acceptance Test with empty scenarios

---
+++ spec/features/flickr_api_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature 'Flickr API Management' do
+
+  scenario 'User goes to Index page and see some photos', js: true do
+  end
+
+  scenario 'User performs a search', js: true do
+  end
+
+  scenario 'User performs a search without match', js: true do
+  end
+
+  scenario 'User clicks on a search result', js: true do
+  end
+
+ end
